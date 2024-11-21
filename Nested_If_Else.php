3cb49a347b7d16d1569b01319061d9c5baaa5d67
Suyash-Marathe <suyashmrth@gmail.com>
2024-11-21 15:00:07 +0530

    Add files via upload

---
+++ Nested_If_Else.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = 10;
+$b = 20;
+$c = 30;
+
+if ($a >= $b) {
+
+    if ($a >= $c) {
+
+        echo "A is Greater.";
+    } else {
+
+        echo "C is Greater.";
+    }
+} else {
+
+    if ($b >= $c) {
+
+        echo "B is Greater.";
+    } else {
+
+        echo "C is Greater.";
+    }
+}
