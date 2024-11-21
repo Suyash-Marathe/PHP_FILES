d4ef243fde5dfbd41de73d7437d2d90b5a6a8c3b
Suyash-Marathe <suyashmrth@gmail.com>
2024-11-21 14:06:54 +0530

    Add files via upload

---
+++ Die_Func.php
@@ -0,0 +1,11 @@
+<?PHP
+
+// die() : die() statement use to return after the executed program.
+
+echo "<p> This is Statement 1.";
+echo "<p> This is Statement 2.";
+
+die();
+
+echo "<p> This is Statement 3.";
+echo "<p> This is Statement 4.";
---
+++ Exit_Func.php
@@ -0,0 +1,11 @@
+<?PHP
+
+// exit() : exit() is used to break the flow of the program.  
+
+echo "<p> This is Statement 1.";
+echo "<p> This is Statement 2.";
+
+exit();
+
+echo "<p> This is Statement 3.";
+echo "<p> This is Statement 4.";
