2db8e5746dd7794e6b508f8944e73bee1a8a147f
Suyash-Marathe <suyashmrth@gmail.com>
2024-11-23 13:20:17 +0530

    Update Cookie_4.php

--- Cookie_4.php
+++ Cookie_4.php
@@ -8,7 +8,7 @@ setcookie($cookie_name, $cookie_pass, time() +36400);
 echo "Name = $cookie_name \n";
 echo "Passward = $cookie_pass \n";
 
-setcookie($cookie_name, $cookie_pass, time () -86400);
+setcookie($cookie_name, $cookie_pass, time () -36400);
 
 echo "Name = $cookie_name \n";
 echo "Passward = $cookie_pass \n";
